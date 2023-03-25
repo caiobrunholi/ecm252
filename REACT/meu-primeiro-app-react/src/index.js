6de239fb707f7d74e7831604a01b50be6736b606
caiobrunholi <caio.brunholi@hotmail.com>
2023-03-25 13:00:30 -0300

    fim aula 5

---
+++ REACT/meu-primeiro-app-react/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles.css'
+const App = () => {
+    const estilosBotao = {
+        marginTop: 12, paddingTop: 8, paddingBottom: 8,
+        backgroundColor: 'blueviolet', color: 'white', border: 'none', width: "100%",
+        borderRadius: 8
+    };
+    return (
+        <div style={{
+            margin: 'auto', width: 768, backgroundColor: '#EEE', padding:
+                12, borderRadius: 8
+        }}>
+            <label className="rotulo" htmlFor="nome" style={{
+                display: 'block',
+                marginBottom: 4
+            }}>Nome:</label>
+            <input type="text" id="nome" style={{
+                paddingTop: 8, paddingBottom: 8,
+                borderStyle: 'hidden', width: '100%', borderRadius: 8, outline: 'none', boxSizing:
+                    'border-box'
+            }} />
+            <button style={estilosBotao}>Enviar</button>
+        </div>
+    )
+}
+
+
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+)
